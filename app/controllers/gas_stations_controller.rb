6598b844adb71b2db01e599323a7b0bddd77083c
Vitor Abreu <vitorabreu.contato@gmail.com>
2025-03-07 00:40:08 -0300

    search func

--- app/controllers/gas_stations_controller.rb
+++ app/controllers/gas_stations_controller.rb
@@ -59,6 +59,11 @@ class GasStationsController < ApplicationController
     end
   end
 
+  def search
+    @name = params[:name]
+    @gas_stations = GasStation.where "name like ?", "%#{@name}%"
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_gas_station
